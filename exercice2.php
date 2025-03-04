622c6bccdacdab01bd78f7383ae5abff8376c0e2
Anthony Laplane <anthony.laplane@gmail.com>
2025-03-04 10:16:12 +0100

    ex2

---
+++ exercice2.php
@@ -0,0 +1,18 @@
+<?php
+$product_name = "Ordinateur portable DELL";
+$price = 1200;
+$description = "Ordinateur DELL avec carte graphique";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nom du produit: <?=$product_name; ?> </h1>
+    <p>Description: <?=$description?></p>
+    <h2>Prix: <?=$price?>€</h2>
+</body>
+</html>
