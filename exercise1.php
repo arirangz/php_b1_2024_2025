f9e6b68feef5fe957beeab4a44e9a82a60abeb88
Anthony Laplane <anthony.laplane@gmail.com>
2025-03-04 10:04:44 +0100

    first commit

---
+++ exercise1.php
@@ -0,0 +1,9 @@
+<?php
+
+$first_name = "John";
+$last_name = "Doe";
+$age=22;
+
+?>
+
+<p><?="Je m'appelle $first_name $last_name. J'ai $age ans.";  ?></p>
---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hello";
+
+$test = "John";
+
+
+?>
+
+<p> <?php echo 'Hello ' . $test . ', ça va ?'; ?> </p>
