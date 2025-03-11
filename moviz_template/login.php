8165f60d3af16f380d87b96e8723ce74b517e2f8
Anthony Laplane <anthony.laplane@gmail.com>
2025-03-11 16:56:51 +0100

    form

---
+++ moviz_template/login.php
@@ -0,0 +1,42 @@
+<?php require_once "templates/header.php" ?>
+<?php 
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+        if ($_POST["email"] === "test@gmail.com" && $_POST["password"] === "ABC123" ) {
+            $isAllowed = true;
+        } else {
+            $isAllowed = false;
+        }
+    } 
+?>
+
+
+<div class="container col-xxl-8 px-4 py-5">
+
+    <form method="post">
+        <?php if (isset($isAllowed)) { ?>
+            <?php if ($isAllowed === true) { ?>
+                <div class="alert alert-success">
+                    Accès autorisé
+                </div>
+            <?php } else {?>
+                <div class="alert alert-danger">
+                    Accès refusé
+                </div>
+            <?php } ?>
+        <?php } ?>
+
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div>
+        <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    </form>
+
+</div>
+<?php require_once "templates/footer.php" ?>
